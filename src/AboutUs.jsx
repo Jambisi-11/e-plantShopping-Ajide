6b5badd62c2f00e0072a21203ba36f4355f6f943
Jambisi-11 <jambis.banji@yahoo.com>
2025-02-06 21:16:25 +0100

    updated

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -4,23 +4,23 @@ import './AboutUs.css';
 function AboutUs() {
   return (
     <div className="about-us-container">
-      {/* <h1 className="about-us-heading">About Us</h1> */}
+       <h1 className="about-us-heading">About Us</h1> 
       <p className="about-us-description">Welcome To More-Of-You Nursery,
       Where Greenery Meets Peace!</p>
       <p className="about-us-content">
-      At More-Of-You Nursery, we are dedicated to bringing nature closer to you. Our mission is to 
-      offer a diverse selection of high-quality plants that not only beautify your space but also 
-      promote a healthier and more sustainable way of living. Whether you're looking for air-purifying
-       greens or fragrant, aromatic plants, we have something to delight every plant lover.
+          At More-Of-You Nursery, we are dedicated to bringing nature closer to you. Our mission is to 
+          offer a diverse selection of high-quality plants that not only beautify your space but also 
+          promote a healthier and more sustainable way of living. Whether you're looking for air-purifying
+          greens or fragrant, aromatic plants, we have something to delight every plant lover.
       </p>
-       <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> 
+       <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='30px' width='30px' alt="" /></p> 
       <p className="about-us-content">
-      Our dedicated team ensures that every plant meets the highest standards of quality and care. 
-      Whether you're a seasoned gardener or just starting your journey with plants, we're here to 
-      support and guide you. Browse our collection, ask questions, and let us help you find the 
-      perfect plant to enhance your home or office.
-       </p>
-      <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> 
+          Our dedicated team ensures that every plant meets the highest standards of quality and care. 
+          Whether you're a seasoned gardener or just starting your journey with plants, we're here to 
+          support and guide you. Browse our collection, ask questions, and let us help you find the 
+          perfect plant to enhance your home or office.
+      </p>
+      <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='30px' width='30px' alt="" /></p> 
 
       <p className="about-us-content">
       Be part of our mission to cultivate a greener, healthier world. Visit More-Of-You Nursery today and bring the beauty of nature right to your doorstep.  
